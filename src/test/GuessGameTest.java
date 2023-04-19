82c9943f98d71b198f6a8b9b3c986b9cab2be2c9
Caerfyre <ninathereseybas@gmail.com>
2023-04-20 02:34:14 +0800

    add more tests

--- src/test/GuessGameTest.java
+++ src/test/GuessGameTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
@@ -15,6 +16,33 @@ public class GuessGameTest {
     public void testIfNumberIsCreated() {
         GuessGame game = new GuessGame();
         assertNotNull(game.createRandomNum());
+    }
+
+    @Test
+    public void checkIfThereIsInput() {
+        GuessGame game = new GuessGame();
+        String input = "50";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertNotNull(game.getUserGuesses());
+    }
+
+    @Test
+    public void checkIfGuessIsCorrect() {
+        GuessGame game = new GuessGame();
+        assertEquals(0, game.checkGuess(50, 50));
+    }
+
+    @Test
+    public void checkIfGuessIsTooHigh() {
+        GuessGame game = new GuessGame();
+        assertEquals(1, game.checkGuess(50, 51));
+    }
+
+    @Test
+    public void checkIfGuessIsTooLow() {
+        GuessGame game = new GuessGame();
+        assertEquals(2, game.checkGuess(50, 49));
     }
 
 }
